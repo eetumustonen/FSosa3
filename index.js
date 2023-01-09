6632a718e2aef8c19638eaca00b8a767ee60ee03
Eetu <eetu.mustonen998@gmail.com>
2023-01-09 19:20:42 +0200

    3.21

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(result => {
       if(result){
@@ -41,6 +41,7 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -51,14 +52,9 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  if(!body.name || !body.number){
-    console.log("name and/or number is missing")
-    return response.status(400).json({
-        error: 'name and/or number is missing'
-    })
-  }
+  
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -66,15 +62,16 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-  const person = {
-    name: body.content,
-    number: body.number
-  }
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  const { name, number } = request.body
+  
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    {new: true, runValidators: true, context: 'query'})
     .then(updatedperson => {
       response.json(updatedperson)
     })
@@ -91,6 +88,10 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).json( {error: error.message })
+  } else if(error.name === 'CustomValidation') {
+    //sopiva viesti virheellisestä puhelinnumerosta
   }
   next(error)
 }
